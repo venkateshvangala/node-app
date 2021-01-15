0475a97c2039bda183db51a3cd4e1451a6eed9a4
Venkatesh Kumar V <venkatesh.v@claraanalytics.com>
2021-01-15 13:28:57 +0530

    user api integration with dynamic template

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
 const app = express();
 const router = express.Router();
 app.use('/', router);
+app.set('view engine', 'html');
+app.engine('html', require('consolidate').underscore);
 const port = 3000;
 
 
+
+
+
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  //__dirname : It will resolve to your project folder.
+  axios.get("https://jsonplaceholder.typicode.com/users")
+  .then(function(data) {
+    console.log(data)
+    res.render(path.join(__dirname + '/index.html'), { users : data});
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 });
+
+
+
+
 
 router.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname + '/about.html'));
